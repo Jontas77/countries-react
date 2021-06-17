f82bf88a53513ab295e6605f494a778f62093d31
Jonathan Heugh <68595133+Jontas77@users.noreply.github.com>
2021-06-17 22:07:32 +0200

    Completed level 1

--- src/components/CountriesAll.js
+++ src/components/CountriesAll.js
@@ -1,31 +1,57 @@
 const CountriesAll = ({ data }) => {
+  let countries = data.map((country) => {
+    const { name, flag, population, region, capital } = country;
+    return (
+      <div className="card border-0">
+        <img className="card-img-top" src={flag} alt="Country Flag" />
+        <div className="card-body">
+          <h5 className="card-title">{name}</h5>
+          <p>
+            <strong>Population: </strong>
+            {population}
+          </p>
+          <p>
+            <strong>Region: </strong>
+            {region}
+          </p>
+          <p>
+            <strong>Capital: </strong>
+            {capital}
+          </p>
+        </div>
+      </div>
+    );
+  });
   return (
-    <main className="card-container">
-      {data.map((country) => {
-        const { name, flag, population, region, capital } = country;
-        return (
-            <div className="card border-0">
-              <img className="card-img-top" src={flag} alt="Country Flag" />
-              <div className="card-body">
-                <h5 className="card-title">{name}</h5>
-                <p>
-                  <strong>Population: </strong>
-                  {population}
-                </p>
-                <p>
-                  <strong>Region: </strong>
-                  {region}
-                </p>
-                <p>
-                  <strong>Capital: </strong>
-                  {capital}
-                </p>
-              </div>
-            </div>
-        );
-      })}
-    </main>
+    <div className="card-container d-flex flex-wrap flex-row justify-content-center">
+        {countries}
+    </div>
   );
 };
 
 export default CountriesAll;
+// {/* <main className="d-flex flex-row flex-wrap">
+// {data.map((country) => {
+//   const { name, flag, population, region, capital } = country;
+//   return (
+//       <div className="card border-0">
+//         <img className="card-img-top" src={flag} alt="Country Flag" />
+//         <div className="card-body">
+//           <h5 className="card-title">{name}</h5>
+//           <p>
+//             <strong>Population: </strong>
+//             {population}
+//           </p>
+//           <p>
+//             <strong>Region: </strong>
+//             {region}
+//           </p>
+//           <p>
+//             <strong>Capital: </strong>
+//             {capital}
+//           </p>
+//         </div>
+//       </div>
+//   );
+// })}
+// </main>  */}
